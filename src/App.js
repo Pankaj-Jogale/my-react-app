72881b4a185e71964c6581e7a443cd79a72d74f6
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-20 17:35:22 +0530

    use-of-user-defined-tags

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 function App() {
   return (
     <div>
-      <h1>hello</h1>
-      <div>hi all</div>
-      <p>
-        Paragraphs are the building blocks of papers. Many students define
-        paragraphs in terms of length: a paragraph is a group of at least five
-        sentences, a paragraph is half a page long, etc. In reality, though, the
-        unity and coherence of ideas among sentences is what constitutes a
-        paragraph.
-      </p>
+      <h1>Using internal function(Person) as tag in these function(App tag)</h1>
+      <Person />
     </div>
   );
 }
 
+function Person() {
+  return (
+    <div>
+      <h1>Person name</h1>
+      <img src="https://picsum.photos/400" alt="" srcset="" />
+      <h1>Person name</h1>
+    </div>
+  );
+}
 export default App;
 
 // export { App };

87f9a22961d9d25ff703b303858e8be9e4652f46
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-20 17:21:42 +0530

    multline-statement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <h1>hello</h1>
+      <div>hi all</div>
+      <p>
+        Paragraphs are the building blocks of papers. Many students define
+        paragraphs in terms of length: a paragraph is a group of at least five
+        sentences, a paragraph is half a page long, etc. In reality, though, the
+        unity and coherence of ideas among sentences is what constitutes a
+        paragraph.
+      </p>
+    </div>
+  );
 }
 
 export default App;

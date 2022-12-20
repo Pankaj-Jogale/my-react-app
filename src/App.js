b9a1f959c6b576a3cdcc3d2f3730b77be5ec86e3
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-20 17:40:30 +0530

    passing parameter(attr) to function(user defined -tag)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ function App() {
   return (
     <div>
       <h1>Using internal function(Person) as tag in these function(App tag)</h1>
-      <Person />
+      <Person name="pankaj " />
+      <Person name="Raj " />
+      <Person name="Rajiv " />
+      <Person name="Raja " />
     </div>
   );
 }
 
-function Person() {
+function Person({ name }) {
   return (
     <div>
-      <h1>Person name</h1>
+      <h1>Person name: {name}</h1>
       <img src="https://picsum.photos/400" alt="" srcset="" />
-      <h1>Person name</h1>
+      <div>
+        Paragraphs are the building blocks of papers. Many students define
+        paragraphs in terms of length: a paragraph is a group of at least five
+        sentences, a paragraph is half a page long, etc. In reality, though, the
+        unity and coherence of ideas among sentences is what constitutes a
+        paragraph.
+      </div>
+      <br />
     </div>
   );
 }

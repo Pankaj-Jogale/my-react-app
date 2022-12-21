f5e654fbaf884f45493f0efec088b6c22497b141
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-21 10:35:46 +0530

    interpoln-at-tag&attribute-lvl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
+  let username = "Pankaj"; //doing interpolation at tag lvl
+  let btvalue = "trail"; //doing interpolation at attribute lvl
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{username}</h1>
+      <input type="button" value={btvalue} />
     </div>
   );
 }
-
 export default App;

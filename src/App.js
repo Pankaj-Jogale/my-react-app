f692db710c2a5ed5fbca1663ffabb175af9a8897
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-21 21:38:47 +0530

    dynamic bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-function App() {
-  //bootstrap styling
+import { useState } from "react";
 
+function App() {
+  //dynamic bootstrap styling
+
+  let [theme, settheme] = useState("danger");
+
+  let makeprimarytheme = () => {
+    theme = "primary";
+    settheme(theme);
+  };
+
+  let makesuccesstheme = () => {
+    theme = "success";
+    settheme(theme);
+  };
+
+  let makeinfotheme = () => {
+    theme = "info";
+    settheme(theme);
+  };
   return (
     <div>
-      <h1 className="bg-success">Pankaj</h1>
+      <h1 className={`bg-${theme}`}>Pankaj</h1>
+      <input
+        className="btn btn-primary"
+        type="button"
+        value="makeprimarytheme"
+        onClick={makeprimarytheme}
+      />
+      <input
+        className="btn btn-success"
+        type="button"
+        value="makesuccesstheme"
+        onClick={makesuccesstheme}
+      />
+      <input
+        className="btn btn-info"
+        type="button"
+        value="makeinfotheme"
+        onClick={makeinfotheme}
+      />
     </div>
   );
 }

f5a813f8bb44bd4dc4af8e44aaf75fa4395755ba
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-22 21:50:58 +0530

    passing event+custom parameter from btn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 
 function App() {
-  //passing custom parameter from btn
+  //passing event+custom parameter from btn
 
   let [theme, settheme] = useState("primary");
-  let clickme = (p1) => {
+  let clickme = (e, p1) => {
     theme = p1;
     settheme(theme);
     console.log(p1);
+    //event para in console
+    console.log(e);
   };
   return (
     <div>
+      {/* comment in tag*/}
       <h1 className={`bg-${theme} text-light p-2`}>hi</h1>
-      <input type="button" value="clickme" onClick={() => clickme("danger")} />
+      <input
+        type="button"
+        value="clickme"
+        onClick={(e) => clickme(e, "danger")}
+      />
     </div>
   );
 }

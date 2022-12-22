843e9dca37a5fcf57419c19aa3e63937fe277331
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-22 22:40:02 +0530

    msg app using map function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
 function App() {
-  //map function
+  //msg app using map function
 
-  let [list] = useState(["Hello", "hii", "chennai", "delhi"]);
+  let [list] = useState([
+    { message: "Hi", messageTime: new Date() },
+    { message: "hello", messageTime: new Date() },
+  ]);
 
   return (
     <div>
       <h1>map demo</h1>
       {list.map((item) => (
-        <div>{item}</div>
+        <div>
+          <div>
+            {item.message}
+            <span>
+              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
+            </span>
+          </div>
+        </div>
       ))}
     </div>
   );

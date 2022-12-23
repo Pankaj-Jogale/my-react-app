af82ae051501300bda088e9f9fcbc270042f139e
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-24 00:59:27 +0530

    Make ajax/api post call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 
 function App() {
+  //Make ajax/api post call
   let [messageList, setMessageList] = useState([]);
   let getallmsg = async () => {
     let url = `http://localhost:3001/messages`;
@@ -12,12 +13,28 @@ function App() {
     //re-render
     setMessageList(messageList);
   };
+
+  let createnewmsg = async () => {
+    let url = `http://localhost:3001/message`;
+    let data = {
+      message: "Hi from react",
+      reply: false,
+    };
+    await axios.post(url, data);
+  };
   return (
     <div>
       <h1>Make ajax/api call</h1>
       <input type="button" value="Make ajax/api call" onClick={getallmsg} />
+      <input
+        type="button"
+        value="Make ajax/api post call"
+        onClick={createnewmsg}
+      />
       {messageList.map((item) => (
-        <div>{item.message}</div>
+        <>
+          <div>{item.message}</div>
+        </>
       ))}
     </div>
   );

16074e06e825944720e65574a0584cec208b8bd6
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-24 12:35:53 +0530

    taking i/p from textbox validation,enterkey

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+
+import { useEffect, useRef, useState } from "react";
 
 //taking i/p from textbox
 function App() {
+  let inputRef = useRef();
+
   useEffect(() => {
     //console.log("i am called");
     getallmsg();
-  }, []);
+  });
 
   let [messageList, setMessageList] = useState([]);
   let getallmsg = async () => {
@@ -29,6 +32,17 @@ function App() {
 
   let createnewmsg = async () => {
     let url = `http://localhost:3001/message`;
+
+    if (!inputRef.current.checkValidity()) {
+      let box = inputRef.current;
+      box.style.border = "4px solid red";
+      // console.log(inputRef.current);
+      // alert("invalid");
+      return;
+    } else {
+      let box = inputRef.current;
+      box.style.border = "inherit";
+    }
     let data = {
       message: message,
       reply: false,
@@ -36,11 +50,24 @@ function App() {
     await axios.post(url, data);
     setmessage("");
   };
+  let checkenter = (e) => {
+    if (e.keyCode === 13) {
+      createnewmsg();
+    }
+  };
 
   return (
     <div>
       <h1>Make ajax/api call</h1>
-      <input type="text" value={message} onChange={changemsg} />
+      <input
+        type="text"
+        value={message}
+        onChange={changemsg}
+        onKeyUp={checkenter}
+        ref={inputRef}
+        required
+        minLength={3}
+      />
       <input type="button" value="add" onClick={createnewmsg} />
       {messageList.map((item, index) => (
         <>

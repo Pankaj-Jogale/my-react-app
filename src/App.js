cfb74a235e35a0b284510341fe0e18147c111211
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-28 00:13:20 +0530

    using Link tag to navigate in multipage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Hello</h1>} />
-      <Route path="/explore" element={<h1>Explore</h1>} />
-      <Route path="*" element={<h1>Page Not Found</h1>} />
-    </Routes>
+    <div>
+      <Link to="/" className="fs-4">
+        Home |
+      </Link>
+      <Link to="/explore"> Explore |</Link>
+      <Link to="/notification"> notification |</Link>
+
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/explore" element={<h1>Explore</h1>} />
+        <Route path="/notification" element={<h1>notification</h1>} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </div>
   );
 }
 

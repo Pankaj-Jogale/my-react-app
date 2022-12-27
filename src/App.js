884fd0a7efbe05cd0246e562d1e2c7e913f1ab8a
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-27 23:54:57 +0530

    simple multipage apl using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import AppBody from "./components/AppBody";
-import AppFooter from "./components/AppFooter";
-import AppHeader from "./components/AppHeader";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <AppHeader />
-      <AppBody />
-      <AppFooter />
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Hello</h1>} />
+      <Route path="/explore" element={<h1>Explore</h1>} />
+      <Route path="*" element={<h1>Page Not Found</h1>} />
+    </Routes>
   );
 }
 

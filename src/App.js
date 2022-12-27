017981e2885e2e66343043db79054f38ebd487e1
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-27 23:10:23 +0530

    writing head,body,footer in separate func and using them as tag in main function

--- src/App.js
+++ src/App.js
@@ -1,109 +1,65 @@
-import axios from "axios";
+import { useState } from "react";
 
-import { useEffect, useRef, useState } from "react";
-
-//taking i/p from textbox
 function App() {
-  let inputRef = useRef();
-  let [message, setmessage] = useState("");
-  let [messageList, setMessageList] = useState([]);
-
-  useEffect(() => {
-    //console.log("i am called");
-    getallmsg();
-  });
-
-  let changemsg = (e) => {
-    message = e.target.value;
-    setmessage(message);
-    //above 2 equal to setmessage(e.target.value);
-  };
-  let getallmsg = async () => {
-    let url = `http://localhost:3001/messages`;
-    let response = await axios.get(url);
-    //console.log(response.data);
-    messageList = [...response.data];
-    //console.log(messageList);
-    //re-render
-    setMessageList(messageList);
-  };
-
-  let createnewmsg = async (reply) => {
-    let url = `http://localhost:3001/message`;
-
-    if (!inputRef.current.checkValidity()) {
-      alert("invalid");
-      return;
-    }
-    let data = {
-      message: message,
-      reply: reply,
-    };
-
-    await axios.post(url, data);
-    setmessage("");
-    getallmsg();
-  };
-  let checkenter = (e) => {
-    if (e.keyCode === 13) {
-      createnewmsg();
-    }
-  };
-
   return (
     <div>
-      <h1 className="bg-dark text-light sticky-top p-2">Message App</h1>
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-6">
-          <div className="d-flex ">
-            <input
-              className="form-control form-control-lg"
-              type="text"
-              value={message}
-              onChange={changemsg}
-              onKeyUp={checkenter}
-              ref={inputRef}
-              required
-              minLength={3}
-            />
-            <input
-              className="btn btn-secondary"
-              type="button"
-              value="add"
-              onClick={() => createnewmsg(false)}
-            />
-            <input
-              className="btn btn-dark"
-              type="button"
-              value="Reply"
-              onClick={() => createnewmsg(true)}
-            />
-          </div>
-        </div>
-      </div>
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
+    </div>
+  );
+}
 
-      {messageList.map((item, index) => (
-        <div className="row justify-content-center border-4">
-          <div className="col-12 col-md-6 border-4 border-dark">
+function AppHeader() {
+  return (
+    <div className="bg-primary text-light p-1 sticky-top d-flex flex-column justify-content-center align-items-center">
+      <h1>Project Book</h1>
+      <marquee>
+        *Paragraphs are the building blocks of papers. Many students define
+        paragraphs in terms of length: a paragraph is a group of at least five
+        sentences, a paragraph is half a page long, etc. In reality, though, the
+        unity and coherence of ideas among sentences is what constitutes a
+        paragraph.
+      </marquee>
+    </div>
+  );
+}
+
+function AppBody() {
+  let [list] = useState([{}, {}, {}, {}, {}, {}, {}, {}]);
+  return (
+    <div className="row " style={{ width: "100%", height: "auto" }}>
+      {list.map((item, index) => (
+        <div key={index} className="col-12 col-md-3 my-2">
+          <div className="card">
+            <img
+              src={`https://picsum.photos/${201 + index}`}
+              alt="image"
+              style={{ height: "300px", objectFit: "cover" }}
+            />
+            <div className="card-header">Card Title</div>
             <div
-              key={index}
-              className={
-                item.reply
-                  ? "d-flex justify-content-end my-1"
-                  : "d-flex justify-content-start my-1"
-              }
+              className="card-body"
+              style={{ height: "150px", overflow: "scroll" }}
             >
-              <div className="badge text-bg-secondary">
-                {item.message}
-                <span className="ms-4">
-                  {new Date(item.messageTime).getHours()}:
-                  {new Date(item.messageTime).getMinutes()}
-                </span>
-              </div>
+              Paragraphs are the building blocks of papers. Many students define
+              paragraphs in terms of length: a paragraph is a group of at least
+              five sentences.Paragraphs are the building blocks of papers. Many
+              students define paragraphs in terms of length
             </div>
           </div>
         </div>
       ))}
+    </div>
+  );
+}
+
+function AppFooter() {
+  return (
+    <div className="bg-secondary d-flex flex-column justify-content-center align-items-center">
+      <div className="fs-2">About Us</div>
+      <div>Follow Us</div>
+      <div>About Us</div>
     </div>
   );
 }

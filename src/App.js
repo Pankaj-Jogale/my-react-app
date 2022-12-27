f66f6f88a3e71b46978dcb4199183fa04d3932e5
Pankaj RJ <pankajjogale3@gmail.com>
2022-12-27 23:21:27 +0530

    writing all function in separate folder components and inport/export to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import AppBody from "./components/AppBody";
+import AppFooter from "./components/AppFooter";
+import AppHeader from "./components/AppHeader";
 
 function App() {
   return (
@@ -10,57 +12,4 @@ function App() {
   );
 }
 
-function AppHeader() {
-  return (
-    <div className="bg-primary text-light p-1 sticky-top d-flex flex-column justify-content-center align-items-center">
-      <h1>Project Book</h1>
-      <marquee>
-        *Paragraphs are the building blocks of papers. Many students define
-        paragraphs in terms of length: a paragraph is a group of at least five
-        sentences, a paragraph is half a page long, etc. In reality, though, the
-        unity and coherence of ideas among sentences is what constitutes a
-        paragraph.
-      </marquee>
-    </div>
-  );
-}
-
-function AppBody() {
-  let [list] = useState([{}, {}, {}, {}, {}, {}, {}, {}]);
-  return (
-    <div className="row " style={{ width: "100%", height: "auto" }}>
-      {list.map((item, index) => (
-        <div key={index} className="col-12 col-md-3 my-2">
-          <div className="card">
-            <img
-              src={`https://picsum.photos/${201 + index}`}
-              alt="image"
-              style={{ height: "300px", objectFit: "cover" }}
-            />
-            <div className="card-header">Card Title</div>
-            <div
-              className="card-body"
-              style={{ height: "150px", overflow: "scroll" }}
-            >
-              Paragraphs are the building blocks of papers. Many students define
-              paragraphs in terms of length: a paragraph is a group of at least
-              five sentences.Paragraphs are the building blocks of papers. Many
-              students define paragraphs in terms of length
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function AppFooter() {
-  return (
-    <div className="bg-secondary d-flex flex-column justify-content-center align-items-center">
-      <div className="fs-2">About Us</div>
-      <div>Follow Us</div>
-      <div>About Us</div>
-    </div>
-  );
-}
 export default App;
---
+++ src/components/AppFooter.js
@@ -0,0 +1,11 @@
+function AppFooter() {
+  return (
+    <div className="bg-secondary d-flex flex-column justify-content-center align-items-center">
+      <div className="fs-2">About Us</div>
+      <div className="fs-3">Follow Us</div>
+      <div className="fs-4">About Us</div>
+    </div>
+  );
+}
+
+export default AppFooter;

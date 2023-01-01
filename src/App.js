9a086fb29c3dfeb280d3e50117fa17ce5dd778ba
Pankaj RJ <pankajjogale3@gmail.com>
2023-01-01 16:39:47 +0530

    take parameterfrom user&passing while calling func

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { deposit, withdraw } from "./redux/store";
 
-//passing parameter while calling function
+//taking parameter from user and passing while calling function
 
 function App() {
   return (
@@ -19,36 +20,57 @@ function Counter1() {
   //let allstate = useSelector((state) => state);
   //console.log(allstate.userAccount.amount);
 
+  let [amount, setAmount] = useState(0);
+
   let { userAccount } = useSelector((state) => state);
+  let handleInputchange = (e) => {
+    // console.log("hi");
+    amount = parseInt(e.target.value);
+
+    setAmount(amount);
+  };
 
   return (
     <div>
       <h1>Counter 1 amount:{userAccount.amount}</h1>
       <input
+        type="number"
+        placeholder="Enter amount to deposit"
+        value={amount}
+        onChange={handleInputchange}
+      />
+      <input
         type="button"
-        value="Deposit-200"
-        onClick={() => dispatch(deposit(200))}
+        value="Deposit"
+        onClick={() => dispatch(deposit(amount))}
       />
     </div>
   );
 }
 
 function Counter2() {
+  let [amount, setAmount] = useState(0);
   let dispatch = useDispatch();
   let { userAccount } = useSelector((state) => state);
+
+  let handleInputchange = (e) => {
+    amount = parseInt(e.target.value);
+    setAmount(amount);
+  };
 
   return (
     <div>
       <h1>Counter 2 amount:{userAccount.amount}</h1>
+      <input type="number" value={amount} onChange={handleInputchange} />
       <input
         type="button"
-        value="Deposit-100"
-        onClick={() => dispatch(deposit(100))}
+        value="Deposit"
+        onClick={() => dispatch(deposit(amount))}
       />
       <input
         type="button"
-        value="withdraw-100"
-        onClick={() => dispatch(withdraw(100))}
+        value="withdraw"
+        onClick={() => dispatch(withdraw(amount))}
       />
     </div>
   );

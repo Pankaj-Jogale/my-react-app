a20439728a4877075b0780b7e88e83c7434e7adb
Pankaj RJ <pankajjogale3@gmail.com>
2023-01-01 17:11:07 +0530

    install react redux &solve collpase bar prob

--- src/components/AppNavLinks.js
+++ src/components/AppNavLinks.js
@@ -1,44 +1,76 @@
 import { Link } from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
 function AppNavLinks() {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
-      <div className="container-fluid ">
-        <Link className="navbar-brand" to="#">
-          Project Book
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container fluid={true}>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">
                 Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/explore">
+              </Nav.Link>
+              <Nav.Link as={Link} to="/explore">
                 Explore
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/notification">
-                Notifications
-              </Link>
-            </li>
-          </ul>
+              </Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/notification">
+                  Notification
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
+        <div className="container-fluid ">
+          <Link className="navbar-brand" to="#">
+            Project Book
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/explore">
+                  Explore
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/notification">
+                  Notifications
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 export default AppNavLinks;

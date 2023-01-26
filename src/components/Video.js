d2d748525950d20633804b7f415770ffd4cd1484
Pankaj RJ <pankajjogale3@gmail.com>
2023-01-26 21:06:04 +0530

    video link embedded from drive

--- src/components/Video.js
+++ src/components/Video.js
@@ -11,6 +11,7 @@ function Video() {
     setList(list);
   };
   Window.onload = videoData();
+
   return (
     <div>
       <h1>VideoLinks</h1>
@@ -20,6 +21,7 @@ function Video() {
             <th>Full Name</th>
             <th>Email</th>
             <th>Password</th>
+            <th>Links</th>
             <th>Modifications(click if want to modify)</th>
           </tr>
         </thead>
@@ -29,6 +31,17 @@ function Video() {
               <td>{item.uname}</td>
               <td>{item.email}</td>
               <td>{item.upass}</td>
+              <td>
+                <a
+                  className="ms-5"
+                  href="https://drive.google.com/file/d/1NO_Wvc38-8GtkUsALWrHbTJm2ErJCmPo/preview"
+                  width="640"
+                  height="480"
+                  allow="autoplay"
+                >
+                  <i class="fa-sharp fa-solid fa-circle-play"></i>
+                </a>
+              </td>
               <td>
                 <a className="me-3" href="update">
                   Update
